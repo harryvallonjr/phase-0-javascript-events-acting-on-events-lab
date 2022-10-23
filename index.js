81f838fbce30f161103ad209af9d737ddb6e92e5
harryvallonjr <115899523+harryvallonjr@users.noreply.github.com>
2022-10-23 23:31:29 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Your code here
+const dodger = document.getElementById("dodger")
+// Function to move Dodger Left
+  function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+//   Function to move Dodger to Right
+function moveDodgerRight(){
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
